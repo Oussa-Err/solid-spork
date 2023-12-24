5217608d0e7ef2917f8de578fdeb7b559fb7e630
Oussa-Err <dexterlecter0@gmail.com>
2023-12-24 16:57:10 +0100

    contact button updated to log in button

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -35,11 +35,42 @@ const Navbar = () => {
         <div className="navbar__btn">
           <button type="button">
             <a
-              href="/contact"
+              href="/signup"
               className="button_top"
               style={{ color: "#000000" }}
             >
-              Contact
+              Se Connecter
+              <svg
+                fill="#323232"
+                width="30px"
+                height="30px"
+                viewBox="0 0 128.00 128.00"
+                id="Layer_1"
+                version="1.1"
+                xmlSpace="preserve"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"
+                stroke="#323232"
+                strokeWidth="5.632"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  stroke="#CCCCCC"
+                  strokeWidth="0.256"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  {" "}
+                  <g>
+                    {" "}
+                    <path d="M30,49c0,18.7,15.3,34,34,34s34-15.3,34-34S82.7,15,64,15S30,30.3,30,49z M90,49c0,14.3-11.7,26-26,26S38,63.3,38,49 s11.7-26,26-26S90,34.7,90,49z"></path>{" "}
+                    <path d="M24.4,119.4C35,108.8,49,103,64,103s29,5.8,39.6,16.4l5.7-5.7C97.2,101.7,81.1,95,64,95s-33.2,6.7-45.3,18.7L24.4,119.4z"></path>{" "}
+                  </g>{" "}
+                </g>
+              </svg>
             </a>
           </button>
         </div>
@@ -69,7 +100,7 @@ const Navbar = () => {
               <a href="/products">Nos Produit</a>
             </li>
             <li>
-              <a href="/login">Se Connecter</a>
+              <a href="/signup">Se Connecter</a>
             </li>
           </div>
         </div>
