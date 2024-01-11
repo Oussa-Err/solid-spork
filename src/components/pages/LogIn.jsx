3993805c0fd791f9e0fc51cbb6f66196ded32352
Oussa-Err <dexterlecter0@gmail.com>
2024-01-11 10:57:40 +0100

    storing jwt token to localStorage

--- src/components/pages/LogIn.jsx
+++ src/components/pages/LogIn.jsx
@@ -12,25 +12,26 @@ const LogIn = () => {
   const navigate = useNavigate();
   const [err, setErr] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const error = useSelector((state) => state.error);
-  const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  const error = useSelector((state) => state.error);
 
   const [input, setInput] = useState({
     email: "",
     password: "",
   });
-
+  console.log(user);
   const loginResponse = async () => {
-    if (user.statut === "success") {
+    if (user.status === "success") {
       console.log("executed on success!");
+      localStorage.setItem(user.user.name, user.token);
       toast.success("logged in successful!");
     }
   };
 
   useEffect(() => {
-    loginResponse();
     setErr(error);
+    loginResponse();
   }, [error, dispatch]);
 
   const handleOnChange = (e) => {
@@ -84,29 +85,27 @@ const LogIn = () => {
             required
           />
           {err && (
-            <div className="">
-              <div className="error-alert">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <svg
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="error-svg"
-                    >
-                      <path
-                        clipRule="evenodd"
-                        d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
-                        fillRule="evenodd"
-                      ></path>
-                    </svg>
-                  </div>
-                  <div className="error-prompt-container">
-                    <p className="error-prompt-heading">
-                      {err.split(":")[2] || err}
-                    </p>
-                  </div>
+            <div className="error-alert">
+              <div className="flex">
+                <div className="flex-shrink-0">
+                  <svg
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="error-svg"
+                  >
+                    <path
+                      clipRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z"
+                      fillRule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="error-prompt-container">
+                  <p className="error-prompt-heading">
+                    {err.split(":")[2] || err}
+                  </p>
                 </div>
               </div>
             </div>
@@ -129,7 +128,7 @@ const LogIn = () => {
             style={{ fontSize: "small", opacity: "70%" }}
             href="/forgetpassword"
           >
-            Forgot password? &nbsp;
+            Forgot password?&nbsp;
           </a>
         </form>
         <ToastContainer />
